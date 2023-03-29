393598d2f9bed90e1b2e29994a018af7fc27a2b5
NomanTB <normantbyou@gmail.com>
2023-03-29 12:22:40 +0300

    new dz

---
+++ homework_5_colorama.py
@@ -0,0 +1,5 @@
+from colorama import *
+print(Fore.RED + 'дз')
+print(Back.CYAN + 'по',Fore.MAGENTA)
+print(Style.NORMAL + 'colorama')
+print(Style.RESET_ALL,'в шаг')
---
+++ homework_6.py
@@ -0,0 +1,68 @@
+# import kem as kem
+#
+# result = []
+#
+# def divider(a, b):
+#     if a < b:
+#         raise ValueError
+#     if b > 100:
+#         raise IndexError
+#     return a/b
+#
+# try:
+#     data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+# except TypeError:
+#     print("строка не делиться")
+#
+#
+# for key in data:
+#     res = divider(key, data[kem])
+#     result.append(res)
+#
+# print(result)
+
+# result = []
+#
+# def divider(a, b):
+#     try:
+#         if a < b:
+#             raise ValueError
+#         if b > 100:
+#             raise IndexError
+#         return a / b
+#     except (ValueError, IndexError) as e:
+#         print("Error")
+#         return None
+#     except Exception as e:
+#         print("Unexpected error")
+#         return None
+#
+# data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
+#
+# for key in data:
+#     res = divider(key, data.get(key))
+#     if res is not None:
+#         result.append(res)
+#
+# print(result)
+
+result = []
+def divider(a, b):
+    if a < b:
+        raise ValueError
+    if b > 100:
+        raise IndexError
+    return a/b
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+
+for key in data:
+    try:
+        res = divider(key, data[key])
+        result.append(res)
+    except ValueError:
+        print("ValueError")
+    except IndexError:
+        print("IndexError")
+
+print(result)
