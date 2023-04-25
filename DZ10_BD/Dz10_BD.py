9bd3c56965337ed8ea505687fae2678078dd4ebf
NomanTB <normantbyou@gmail.com>
2023-04-25 11:01:15 +0300

    new

--- DZ10_BD/Dz10_BD.py
+++ DZ10_BD/Dz10_BD.py
@@ -2,29 +2,24 @@ from bs4 import BeautifulSoup
 import requests,lxml,sqlite3 as sq
 
 def code_osnova():
-    with sq.connect('pogoda.sl3', timeout=5) as con:
+    with sq.connect('POGODA.sl3', timeout=5) as con:
         cur = con.cursor()
-        # cur.execute('''CREATE TABLE IF NOT EXISTS POGODA(
-        # id INTEGER PRIMARY KEY AUTOINCREMENT,
-        # time INTEGER DEFAULT "НЕ ИЗВЕСНО",
-        # temperature INTEGER DEFAULT "НЕ ИЗВЕСНО"
-        # )''')
-    headers = {
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
-    }
-    html = requests.get('https://pogoda.meta.ua/world/ugorshhina/p-tf-rd-/month/',timeout=5)
+        cur.execute('''CREATE TABLE IF NOT EXISTS Roma(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        time INTEGER DEFAULT "НЕ ИЗВЕСНО",
+        pogodazp TEXT DEFAULT "НЕ ИЗВЕСНО"
+        )''')
+
+    html = requests.get('https://meteoexpert.org/ru/forecast10/zaporizhzhia/',timeout=100)
     soup = BeautifulSoup(html.text,'lxml')
 
-    print(html.status_code)
-    # print(soup.select('.fl:nth-child(1)'))
-    print(soup.get('.fl:nth-child(3) '))
-    # print(html.text)
 
 
-
-
-    # a = soup.select('div .month__wrap')
-    # print(a)
+    for c, i in zip(soup.select('.weekday'),soup.select(".desc")):
+        print(c.text,'   ', i.text)
+        cur.execute(f'''
+                    INSERT INTO Roma (time, pogodazp) VALUES ('{c.text}', '{i.text}');
+                    ''')
 
 
 if __name__ == "__main__":
