7699d6b63394199156856e590d00554fdce527ea
NomanTB <normantbyou@gmail.com>
2023-04-26 09:43:06 +0300

    new

--- DZ10_BD/Dz10_BD.py
+++ DZ10_BD/Dz10_BD.py
@@ -1,26 +1,43 @@
 from bs4 import BeautifulSoup
 import requests,lxml,sqlite3 as sq
 
-def code_osnova():
-    with sq.connect('POGODA.sl3', timeout=5) as con:
-        cur = con.cursor()
-        cur.execute('''CREATE TABLE IF NOT EXISTS Roma(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        time INTEGER DEFAULT "НЕ ИЗВЕСНО",
-        pogodazp TEXT DEFAULT "НЕ ИЗВЕСНО"
-        )''')
+class DZ:
+    def __init__(self):
+        html = requests.get('https://meteoexpert.org/ru/forecast10/zaporizhzhia/', timeout=100)
+        soup = BeautifulSoup(html.text, 'lxml')
 
-    html = requests.get('https://meteoexpert.org/ru/forecast10/zaporizhzhia/',timeout=100)
-    soup = BeautifulSoup(html.text,'lxml')
+        for c, i in zip(soup.select('.weekday'), soup.select(".desc")):
+            self.c = c
+            self.i = i
+            print(c.text, '   ', i.text)
+            # cur.execute(f'''
+            #             INSERT INTO POGODA (time, pogodazp) VALUES ('{c.text}', '{i.text}');
+            #             ''')
 
 
+    def code_osnova(self):
+        with sq.connect('POGODA.sl3', timeout=5) as con:
+            cur = con.cursor()
+            cur.execute('''CREATE TABLE IF NOT EXISTS POGODA(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            time INTEGER DEFAULT "НЕ ИЗВЕСНО",
+            pogodazp TEXT DEFAULT "НЕ ИЗВЕСНО"
+            )''')
+            p = self.c
+            p1 = p.text
 
-    for c, i in zip(soup.select('.weekday'),soup.select(".desc")):
-        print(c.text,'   ', i.text)
-        cur.execute(f'''
-                    INSERT INTO Roma (time, pogodazp) VALUES ('{c.text}', '{i.text}');
+            g = self.i
+            g1 = g.text
+            cur.execute(f'''
+                    INSERT INTO POGODA (time, pogodazp) VALUES ('{p1}', '{g1}');
                     ''')
 
 
+
+
+
+
 if __name__ == "__main__":
-    code_osnova()
+    d = DZ()
+    d.code_osnova()
+    # code_osnova()
